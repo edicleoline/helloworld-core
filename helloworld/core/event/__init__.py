c91741d24e2685dd4bc73c81d108c900af6eb2ac
edicleoline <edicleo@proton.me>
2024-09-22 15:33:03 -0300

    Architecture alpha 0.8

--- helloworld/core/event/__init__.py
+++ helloworld/core/event/__init__.py
@@ -4,4 +4,4 @@ from .api import contains as contains
 from .api import listen as listen
 from .api import listens_for as listens_for
 from .api import remove as remove
-from .base import Events as Events, AsyncEvents as AsyncEvents
+from .base import Events as Events
--- helloworld/core/event/base.py
+++ helloworld/core/event/base.py
@@ -1,41 +1,8 @@
 from __future__ import annotations
 
 from typing import List, Any, Tuple, Callable
-import asyncio
 
 from reactivex.subject import Subject
-from reactivex import operators as ops
-
-class AsyncEvents:
-    _signals: List[Tuple[str, Subject]]
-
-    def __init__(self):
-        self._signals = []
-
-    async def register_event(self, event_name: str) -> None:
-        if not any(event for event, _ in self._signals if event == event_name):
-            signal = Subject()
-            self._signals.append((event_name, signal))
-
-    async def trigger_event(self, event_name: str, **kwargs: Any) -> None:
-        for event, signal in self._signals:
-            if event == event_name:
-                await asyncio.sleep(0)
-                signal.on_next(kwargs)
-                return
-        print(f"Event '{event_name}' not registered.")
-
-    async def listen(self, event_name: str, observer: Callable[..., Any]) -> None:
-        for event, signal in self._signals:
-            if event == event_name:
-                async def async_observer(data):
-                    await asyncio.sleep(0)
-                    observer(data)
-
-                signal.pipe(ops.map(lambda x: asyncio.create_task(async_observer(x)))).subscribe()
-                return
-        print(f"Event '{event_name}' not registered, cannot listen.")
-
 
 class Events:
     _signals: List[Tuple[str, Subject]]
